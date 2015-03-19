85e1e896d97849ab3c04350500a3ba20f3f5cbe9
ririthevent <ririthevent@gmail.com>
2015-03-19 18:32:28 +0900

    question
    question

---
+++ script.js
@@ -0,0 +1,21 @@
+//var i = 1;
+//for(i=1;i<=100;i++){
+//  console.log(i);
+//  document.getElementById('debug').innerHTML += i + "<br>";
+//}
+
+
+//var num, total = 0;
+
+/*for(var isEnd=false; isEnd===false;){
+  var num = prompt("숫자를 입력하세요");
+  if(num === null){
+    isEnd=true;
+    alert("합은"+total+"입니다");
+  }
+  else{
+    total += parseInt(num);
+  }
+}*/
+
+var btn = document.getElementById('btn');
